161cdeb063aa1735862af89aa584ebfb09d190d7
JeyDevLab <jey@JeyDevLab.info>
2024-12-04 08:14:12 -0500

    Replace assetprefix with our domain name

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -4,6 +4,6 @@ import { pluginReact } from '@rsbuild/plugin-react';
 export default defineConfig({
   plugins: [pluginReact()],
   output: {
-    assetPrefix: 'https://jeydevlab.github.io/website/',
+    assetPrefix: 'https://jeydevlab.info/',
   },
 });
