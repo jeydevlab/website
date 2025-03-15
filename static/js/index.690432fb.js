55dee19e883fdf4bfd01490f67bda104c330d33b
JeyDevLab <jey@JeyDevLab.info>
2025-03-15 13:50:46 -0400

    Add linux version

---
+++ static/js/index.690432fb.js
@@ -0,0 +1,24 @@
+(()=>{"use strict";var e={127:function(e,r,n){var i=n("893"),t=n("294"),s=n("745"),a=n("776"),o=n("329");let d=o.ZP.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+	justify-content: space-evenly;
+	margin-left: 25%;
+	width: 50%;
+		
+	img {
+			border-radius: 50%;
+			height: 128px;
+			width: 128px;
+
+			padding: 5px;
+	}
+		
+	a {
+			text-decoration: none;
+			
+			:active {
+					
+			}
+	}
+`;function c(){return(0,i.jsxs)(d,{children:[(0,i.jsx)(a.rU,{to:"/",children:"Home"}),(0,i.jsx)(a.rU,{to:"/videos",children:"Videos"}),(0,i.jsx)("img",{src:"https://jeydevlab.info/images/coding_logo_rect_640.png",alt:"Logo image"}),(0,i.jsx)(a.rU,{to:"/download",children:"Download"}),(0,i.jsx)(a.rU,{to:"/about",children:"About"})]})}let l="https://jeydevlab.info";n("658"),n("97"),document.title="JeyDevLab",s.createRoot(document.getElementById("root")).render((0,i.jsx)(t.StrictMode,{children:(0,i.jsx)(a.UT,{children:(0,i.jsx)(a.Z5,{children:(0,i.jsxs)(a.AW,{element:(0,i.jsx)(()=>(0,i.jsxs)("div",{className:"content",children:[(0,i.jsx)(c,{}),(0,i.jsx)(a.j3,{})]}),{}),children:[(0,i.jsx)(a.AW,{index:!0,element:(0,i.jsx)("div",{children:"welcome"})}),(0,i.jsx)(a.AW,{path:"download",element:(0,i.jsx)(function(){return(0,i.jsx)(i.Fragment,{children:(0,i.jsx)("article",{className:"no-padding medium-width small-height",children:(0,i.jsxs)("div",{className:"grid no-space",children:[(0,i.jsx)("div",{className:"s6",children:(0,i.jsx)("img",{className:"responsive padding",src:`${l}/images/minecraftserver_thumbnail.png`,alt:"Minecraft Web server"})}),(0,i.jsx)("div",{className:"s6",children:(0,i.jsxs)("div",{className:"padding",children:[(0,i.jsx)("h5",{children:"Minecraft web server"}),(0,i.jsx)("p",{children:"Version: 1.0.0"}),(0,i.jsxs)("nav",{children:[(0,i.jsx)("a",{href:`${l}/apps/MinecraftWebServer-1.0.0-Win.zip`,download:"minecraft-webserver-windows",children:(0,i.jsx)("button",{className:"border primary square",children:(0,i.jsx)("img",{src:`${l}/images/windows.svg`,alt:"Window app"})})}),(0,i.jsx)("a",{href:`${l}/apps/MinecraftWebServerLinux-1.0.0.tar.gz`,download:"minecraft-webserver-linux",children:(0,i.jsx)("button",{className:"border primary square",children:(0,i.jsx)("img",{src:`${l}/images/linux.svg`,alt:"Linux app"})})})]})]})})]})})})},{})}),(0,i.jsx)(a.AW,{path:"videos",element:(0,i.jsx)("div",{children:"Videos"})}),(0,i.jsx)(a.AW,{path:"about",element:(0,i.jsx)("div",{children:"about"})}),(0,i.jsx)(a.AW,{path:"gaming",element:(0,i.jsx)("div",{children:"gaming"})})]})})})}))}},r={};function n(i){var t=r[i];if(void 0!==t)return t.exports;var s=r[i]={exports:{}};return e[i](s,s.exports,n),s.exports}n.m=e,n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,{a:r}),r},n.d=function(e,r){for(var i in r)n.o(r,i)&&!n.o(e,i)&&Object.defineProperty(e,i,{enumerable:!0,get:r[i]})},n.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},n.nc=void 0,(()=>{var e=[];n.O=function(r,i,t,s){if(i){s=s||0;for(var a=e.length;a>0&&e[a-1][2]>s;a--)e[a]=e[a-1];e[a]=[i,t,s];return}for(var o=1/0,a=0;a<e.length;a++){for(var i=e[a][0],t=e[a][1],s=e[a][2],d=!0,c=0;c<i.length;c++)(!1&s||o>=s)&&Object.keys(n.O).every(function(e){return n.O[e](i[c])})?i.splice(c--,1):(d=!1,s<o&&(o=s));if(d){e.splice(a--,1);var l=t();void 0!==l&&(r=l)}}return r}})(),n.rv=function(){return"1.1.4"},(()=>{var e={980:0};n.O.j=function(r){return 0===e[r]};var r=function(r,i){var t=i[0],s=i[1],a=i[2],o,d,c=0;if(t.some(function(r){return 0!==e[r]})){for(o in s)n.o(s,o)&&(n.m[o]=s[o]);if(a)var l=a(n)}for(r&&r(i);c<t.length;c++)d=t[c],n.o(e,d)&&e[d]&&e[d][0](),e[d]=0;return n.O(l)},i=self.webpackChunkwebsite=self.webpackChunkwebsite||[];i.forEach(r.bind(null,0)),i.push=r.bind(null,i.push.bind(i))})(),n.ruid="bundler=rspack@1.1.4";var i=n.O(void 0,["361","118","130"],function(){return n("127")});i=n.O(i)})();
